be881ecbe0ca794b82124d715d19f39ab87538a8
Simmigon Flagg <simmigon.flagg@gmail.com>
2022-05-11 15:05:43 -0400

    Adding Auto

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "kaipherweb2180fd81": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
